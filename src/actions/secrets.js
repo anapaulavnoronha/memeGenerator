24f1b729f96497954f10b36e0740646a522ec774
Ana <a.vargas.de.noronha@accenture.com>
2020-09-03 23:45:42 +0100

    Creating redux generator project structure

---
+++ src/actions/secrets.js
@@ -0,0 +1,7 @@
+//why here in actions folder?
+//actions will be the only one using this information
+
+const username = "anapaulavnoronha";
+const password = "paulinhavn";
+
+export { username, password };
